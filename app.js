a72ecaf8a09da1cbda42417eefcd2a478ffba843
niazrahman <b170403065@sw.jnu.ac.bd>
2021-06-16 18:58:54 +0600

    post request handling

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const express = require('express');
 
 const app = express();
-
+app.use(express.json());
 // app.get('/', (req, res) => {
 //     res.status(200).send('Hello from the server');
 // });
@@ -26,6 +26,23 @@ app.get('/api/v1/tours', (req, res) => {
     });
 });
 
+app.post('/api/v1/tours', (req, res) => {
+    const newId = tours[tours.length - 1].id + 1;
+    const newTour = Object.assign({ id: newId }, req.body);
+    tours.push(newTour);
+    fs.writeFile(
+        `${__dirname}/starter/dev-data/data/tours-simple.json`,
+        JSON.stringify(tours),
+        (err) => {
+            res.status(201).json({
+                status: 'success',
+                data: {
+                    tour: newTour,
+                },
+            });
+        }
+    );
+});
 const port = 3000;
 app.listen(port, () => {
     console.log(`App is running on port ${port}`);
