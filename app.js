e27e6fa0845347a40c11cdff4b926f0001099ed7
niazrahman <b170403065@sw.jnu.ac.bd>
2021-06-16 13:34:57 +0600

    api handling get request

--- app.js
+++ app.js
@@ -1,15 +1,28 @@
+const fs = require('fs');
 const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.status(200).send('Hello from the server');
-});
+// app.get('/', (req, res) => {
+//     res.status(200).send('Hello from the server');
+// });
 
-app.post('/', (req, res) => {
+// app.post('/', (req, res) => {
+//     res.status(200).json({
+//         message: 'You can post at the endPoint...',
+//         app: 'Node Project',
+//     });
+// });
+const tours = JSON.parse(
+    fs.readFileSync(`${__dirname}/starter/dev-data/data/tours-simple.json`)
+);
+app.get('/api/v1/tours', (req, res) => {
     res.status(200).json({
-        message: 'You can post at the endPoint...',
-        app: 'Node Project',
+        status: 'success',
+        results: tours.length,
+        data: {
+            tours,
+        },
     });
 });
 
