5765a331e60e39b9c29bf8c3774f3c2d0da2d55f
niazrahman <b170403065@sw.jnu.ac.bd>
2021-06-17 14:43:06 +0600

    URL parameters responded

--- app.js
+++ app.js
@@ -25,6 +25,23 @@ app.get('/api/v1/tours', (req, res) => {
         },
     });
 });
+app.get('/api/v1/tours/:id', (req, res) => {
+    console.log(req.params);
+    const id = req.params.id * 1; //NOTE: converts string into number
+    const tour = tours.find((el) => el.id === id);
+    if (!tour) {
+        return res.status(404).json({
+            status: 'Failed',
+            message: 'ID not found!',
+        });
+    }
+    res.status(200).json({
+        status: 'success',
+        data: {
+            tour,
+        },
+    });
+});
 
 app.post('/api/v1/tours', (req, res) => {
     const newId = tours[tours.length - 1].id + 1;
@@ -35,10 +52,8 @@ app.post('/api/v1/tours', (req, res) => {
         JSON.stringify(tours),
         (err) => {
             res.status(201).json({
-                status: 'success',
-                data: {
-                    tour: newTour,
-                },
+                status: 'Success',
+                tour: newTour,
             });
         }
     );
