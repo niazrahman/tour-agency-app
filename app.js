7837d9962d5f92bb8a13331534931736d699ecb9
niazrahman <b170403065@sw.jnu.ac.bd>
2021-06-21 00:53:23 +0600

    set up environment varriables

--- app.js
+++ app.js
@@ -5,7 +5,11 @@ const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 // 1.MIDDLEWARE
 app.use(express.json());
-app.use(morgan('dev'));
+console.log(process.env.NODE_ENV);
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+}
+
 app.use(express.static(`${__dirname}/starter/public`));
 app.use((req, res, next) => {
     console.log('Hello from the middleware');
