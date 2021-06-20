5954c4b3a866a9e8611ad94676594acfc5121bb1
niazrahman <b170403065@sw.jnu.ac.bd>
2021-06-20 22:20:26 +0600

    using controllers

---
+++ controllers/tourController.js
@@ -0,0 +1,77 @@
+const fs = require('fs');
+
+const tours = JSON.parse(
+    fs.readFileSync(`${__dirname}/../starter/dev-data/data/tours-simple.json`)
+);
+
+exports.getAllTours = (req, res) => {
+    res.status(200).json({
+        status: 'success',
+        results: tours.length,
+        data: {
+            tours,
+        },
+    });
+};
+exports.getTour = (req, res) => {
+    console.log(req.params);
+    const id = req.params.id * 1; //NOTE: converts string into number
+    const tour = tours.find((el) => el.id === id);
+    if (!tour) {
+        return res.status(404).json({
+            status: 'Failed',
+            message: 'ID not found!',
+        });
+    }
+    res.status(200).json({
+        status: 'success',
+        requestedAt: req.requestTime,
+        data: {
+            tour,
+        },
+    });
+};
+exports.createTour = (req, res) => {
+    const newId = tours[tours.length - 1].id + 1;
+    const newTour = Object.assign({ id: newId }, req.body);
+    tours.push(newTour);
+    fs.writeFile(
+        `${__dirname}/starter/dev-data/data/tours-simple.json`,
+        JSON.stringify(tours),
+        (err) => {
+            res.status(201).json({
+                status: 'Success',
+                tour: newTour,
+            });
+        }
+    );
+};
+exports.updateTour = (req, res) => {
+    const id = req.params.id * 1;
+    if (id > tours.length) {
+        return res.status(404).json({
+            status: 'Failed',
+            message: 'ID Not Found',
+        });
+    }
+
+    res.status(200).json({
+        status: 'Success',
+        data: {
+            tour: '<Updated Tour Here...>',
+        },
+    });
+};
+exports.deleteTour = (req, res) => {
+    const id = req.params.id * 1;
+    if (id > tours.length) {
+        return res.status(404).json({
+            status: 'Failed',
+            message: 'ID Not Found',
+        });
+    }
+    res.status(204).json({
+        status: 'Success',
+        data: null,
+    });
+};
--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,82 +1,16 @@
-const fs = require('fs');
 const express = require('express');
-const tours = JSON.parse(
-    fs.readFileSync(`${__dirname}/../starter/dev-data/data/tours-simple.json`)
-);
-const getAllTours = (req, res) => {
-    res.status(200).json({
-        status: 'success',
-        results: tours.length,
-        data: {
-            tours,
-        },
-    });
-};
-const getTour = (req, res) => {
-    console.log(req.params);
-    const id = req.params.id * 1; //NOTE: converts string into number
-    const tour = tours.find((el) => el.id === id);
-    if (!tour) {
-        return res.status(404).json({
-            status: 'Failed',
-            message: 'ID not found!',
-        });
-    }
-    res.status(200).json({
-        status: 'success',
-        requestedAt: req.requestTime,
-        data: {
-            tour,
-        },
-    });
-};
-const createTour = (req, res) => {
-    const newId = tours[tours.length - 1].id + 1;
-    const newTour = Object.assign({ id: newId }, req.body);
-    tours.push(newTour);
-    fs.writeFile(
-        `${__dirname}/starter/dev-data/data/tours-simple.json`,
-        JSON.stringify(tours),
-        (err) => {
-            res.status(201).json({
-                status: 'Success',
-                tour: newTour,
-            });
-        }
-    );
-};
-const updateTour = (req, res) => {
-    const id = req.params.id * 1;
-    if (id > tours.length) {
-        return res.status(404).json({
-            status: 'Failed',
-            message: 'ID Not Found',
-        });
-    }
+const tourController = require('./../controllers/tourController');
 
-    res.status(200).json({
-        status: 'Success',
-        data: {
-            tour: '<Updated Tour Here...>',
-        },
-    });
-};
-const deleteTour = (req, res) => {
-    const id = req.params.id * 1;
-    if (id > tours.length) {
-        return res.status(404).json({
-            status: 'Failed',
-            message: 'ID Not Found',
-        });
-    }
-    res.status(204).json({
-        status: 'Success',
-        data: null,
-    });
-};
 const router = express.Router();
 
-router.route('/').get(getAllTours).post(createTour);
-router.route('/:id').get(getTour).patch(updateTour).delete(deleteTour);
+router
+    .route('/')
+    .get(tourController.getAllTours)
+    .post(tourController.createTour);
+router
+    .route('/:id')
+    .get(tourController.getTour)
+    .patch(tourController.updateTour)
+    .delete(tourController.deleteTour);
 
 module.exports = router;
