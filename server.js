cc45cf43084fdcb79fe0c98afd4971614cc60ccf
niazrahman <b170403065@sw.jnu.ac.bd>
2021-08-09 00:51:09 +0600

    Database created and connect mongodb using mongoose

--- server.js
+++ server.js
@@ -1,6 +1,22 @@
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
+
+dotenv.config({path:'./config.env'})
+
 const app = require('./app')
 
-const PORT = 3000;
-app.listen(PORT, () => {
-  console.log(`App is running on port ${PORT}...`);
+const DB = process.env.DATABASE.replace('<PASSWORD>',process.env.DATABASE_PASSWORD)
+
+mongoose.connect(DB,{
+  useNewUrlParser : true,
+  useCreateIndex : true,
+  useUnifiedTopology : true,
+  useFindAndModify : false
+}).then(() => {
+  console.log('Database Connected...')
+})
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`App is running on port ${port}...`);
 });
