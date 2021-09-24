753c1bde9b4ca91359514476ae0fd1dab2a8c7cd
niazrahman <b170403065@sw.jnu.ac.bd>
2021-09-24 11:47:28 +0600

    tour api aggregation

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -101,5 +101,44 @@ exports.getTopFive = (req,res,next) => {
         message : err
       })
     }
-    
+
   };
+
+  exports.getTourStats = async (req,res) => {
+    try{
+      const stats  = await Tour.aggregate([
+        {
+          $match : {ratingsAverage : { $gte : 4.5} }
+        },
+        {
+          $group : {
+            _id : {$toUpper : '$difficulty'},
+            numTours : { $sum : 1 },
+            numRatings : { $sum : '$ratingsQuantity' },
+            avgRating : { $avg : '$ratingsAverage' },
+            avgPrice : { $avg : '$price'},
+            minPrice : { $min : '$price' },
+            maxPrice : { $max : '$price' }
+
+          }
+        },
+        {
+          
+            $sort : {avgPrice : 1}
+          
+        }
+
+      ])
+      res.status(200).json({
+        status : 'Success',
+        data : {
+          stats
+        }
+      })
+    }catch(err){
+      res.status(400).json({
+        status : 'Failed',
+        message : err
+      })
+    }
+  }
