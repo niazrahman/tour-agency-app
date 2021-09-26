ee58f411c8fba37ad20f20f8ebd472583c70814b
niazrahman <b170403065@sw.jnu.ac.bd>
2021-09-26 13:34:47 +0600

    error handled in a better way

--- app.js
+++ app.js
@@ -2,8 +2,11 @@ const express = require('express');
 
 const morgan = require('morgan')
 
+const globarErrorHandler = require('./controllers/errorController')
+const AppError = require('./utils/appError');
 const tourRouter = require('./routes/tourRoutes')
-const userRouter = require('./routes/userRoutes')
+const userRouter = require('./routes/userRoutes');
+
 
 const app = express();
 // Middlewares
@@ -18,25 +21,8 @@ app.use('/api/v1/tours',tourRouter)
 app.use('/api/v1/users',userRouter)
 
 app.all('*',(req,res,next) => {
-    // res.status(404).json({
-    //     status: 'Failed',
-    //     message : `Can't find ${req.originalUrl} on this server`
-    // })
-
-    const err = new Error(`Can't find ${req.originalUrl} on this server`)
-    
-    err.status = 'Failed'
-    err.statusCode = 404
-
-    next(err)
+    next(new AppError(`Can't find ${req.originalUrl} on this server`,404))
 })
 
-app.use((err,req,res,next) => {
-    err.statusCode = err.statusCode || 500
-    err.status = err.status || 'error'
-    res.status(err.statusCode).json({
-        status : err.status,
-        message : err.message
-    })
-})
+app.use(globarErrorHandler)
 module.exports = app
