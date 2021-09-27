f8b954212284f10f723fcb1f553442de07739151
niazrahman <b170403065@sw.jnu.ac.bd>
2021-09-27 13:39:40 +0600

    separate development and production error

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,8 +1,39 @@
+const sendErrDev = ((res,err) => {
+    res.status(err.statusCode).json({
+        status : err.status,
+        error : err,
+        message : err.message,
+        stack : err.stack
+    })
+})
+
+const sendErrProd = ((res,err) => {
+    // Operational ,Trusted error:  Send message to the client
+    if(err.isOperational){
+        res.status(err.statusCode).json({
+            status : err.status,
+            message : err.message
+        })
+    // Programming or unknown error : Don't leak error details
+    }else {
+        // 1) log error
+        console.error('Error',err)
+
+        // 2) send a generic message to the client
+        res.status(500).json({
+            status : 'error',
+            message : 'Something went very wrong'
+        })
+    }
+})
+
+
 module.exports = ((err,req,res,next) => {
     err.statusCode = err.statusCode || 500
     err.status = err.status || 'error'
-    res.status(err.statusCode).json({
-        status : err.status,
-        message : err.message
-    })
+    if(process.env.NODE_ENV === 'development'){
+        sendErrDev(res,err)
+    }else if(process.env.NODE_ENV === 'production'){
+        sendErrProd(res,err)
+    }
 })
