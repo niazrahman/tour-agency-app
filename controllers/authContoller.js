72763417ab1aee3e994b59eaf60c9a752ecd67e5
niazrahman <b170403065@sw.jnu.ac.bd>
2021-10-01 14:51:13 +0600

    create jwt token to singing up user

--- controllers/authContoller.js
+++ controllers/authContoller.js
@@ -1,15 +1,22 @@
+const jwt = require('jsonwebtoken')
+
 const User = require('../models/userModel')
 const catchAsync = require('../utils/catchAsync')
 
 exports.signUp = catchAsync(async (req,res,next) => {
     const newUser = await User.create({
         name : req.body.name,
-        emali : req.body.email,
+        email : req.body.email,
         password : req.body.password,
         passwordConfirm : req.body.passwordConfirm
     })
+
+    const token = jwt.sign({ id : newUser._id},process.env.JWT_SECRET_KEY,{
+        expiresIn : process.env.JWT_EXPIRES_IN
+    })
     res.status(201).json({
         status : 'Success',
+        token,
         data : {
             user : newUser
         }
