c95a92af4de2d73333faf8827d98863c0ed8d78c
niazrahman <b170403065@sw.jnu.ac.bd>
2021-10-02 20:02:53 +0600

    check if the user still exists in protect controller

--- controllers/authContoller.js
+++ controllers/authContoller.js
@@ -63,7 +63,12 @@ exports.protect = catchAsync( async (req,res,next) => {
 
     //  2) Verification token
     const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET_KEY)
-    console.log(decoded)
+
+    // check if user still exists
+    const currentUser = await User.findById(decoded.id)
+    if(!currentUser){
+        return next(new AppError('The user belonging to this token does no longer exists!', 401))
+    }
 
     next()
 })
